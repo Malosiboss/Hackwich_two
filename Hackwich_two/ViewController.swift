543d09e37ec5e29949f909a8b319279b284ed2e5
NoahNua <NoahNua@hawaii.edu>
2021-01-26 10:31:23 -1000

    Finished part 1-7

--- Hackwich_two/ViewController.swift
+++ Hackwich_two/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //anything code that you put into the viewDidLoad block is the initial setting for the application
+    }
+//part 7 - we are going to create our first IB action
+    
+    
+    @IBAction func pressMeButtonpressed(_ sender: Any)
+    {
+        //    this is the code block fro pressMeButtonPressed function
+            
+        //    part 7 number 5
+            
+        print("Hello World")
     }
 
-
+    
+    
+    
+    
+    
+   
 }
 
