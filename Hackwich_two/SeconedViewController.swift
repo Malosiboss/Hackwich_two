043d843c90f807456bf25cc631ea2b77ba5423bf
NoahNua <NoahNua@hawaii.edu>
2021-01-28 11:09:36 -1000

    “Completed Parts 1-7”
    Changed label text and color upon button press

--- Hackwich_two/SeconedViewController.swift
+++ Hackwich_two/SeconedViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class SeconedViewController: UIViewController {
-
+  
+    @IBOutlet var firstLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,11 +20,14 @@ class SeconedViewController: UIViewController {
     {
     
         self.view.backgroundColor = UIColor.blue
-    
-    
+       
+        self.firstLabel.text = "I Did It"
+        firstLabel.textColor = UIColor(red: 1.0, green: 0.004, blue: 0.502, alpha: 1.0)
+       
     }
 //    code block^
 //    change background color of the button when the "change color" button is pressed
+    
     
     
     
