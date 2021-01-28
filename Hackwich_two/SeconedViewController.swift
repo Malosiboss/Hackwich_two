625d9506818f96151a52e2f0cfc5125851116335
NoahNua <NoahNua@hawaii.edu>
2021-01-28 10:18:52 -1000

    “Completed Parts 4-6”

---
+++ Hackwich_two/SeconedViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SeconedViewController.swift
+//  Hackwich_two
+//
+//  Created by Noah Nua on 1/28/21.
+//
+
+import UIKit
+
+class SeconedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func changeColorButtonPressed(_ sender: Any)
+    {
+    
+        self.view.backgroundColor = UIColor.blue
+    
+    
+    }
+//    code block^
+//    change background color of the button when the "change color" button is pressed
+    
+    
+    
+    
+
+}
